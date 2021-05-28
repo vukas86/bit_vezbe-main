748f2c1614397667d9b50abbd4440b96ef0c1bca
“BIT <vukasbit@gmail.com>
2021-05-28 17:44:44 +0200

    DOM practice

--- DOM/main.js
+++ DOM/main.js
@@ -23,7 +23,7 @@ findLi()
 colorLi() */
 
 
-function removeClass() {
+/* function removeClass() {
     var removeC = document.getElementById("hello").classList.remove('active');
     console.log(removeC.parentElement)
 }
@@ -31,4 +31,45 @@ function removeClass() {
 
 removeClass()
 
-console.log(removeC.parenElement)
+console.log(removeC.parenElement) */
+
+/* var m = document.querySelector("ul li a").textContent;
+
+
+alert(m); */
+
+/* function changeText(text) {
+    var b = document.getElementsByClassName('lists')[0].lastElementChild.textContent = text;
+}
+
+changeText("hello world"); */
+var beer = ["dark", "pills", "german", "local"];
+var data = document.querySelector(".abou");
+
+function buildList(arr, node) {
+    var select = document.createElement('select');
+
+    for (var i = 0; i < arr.length; i++) {
+        var option = document.createElement('option');
+        option.textContent = arr[i];
+        select.appendChild(option);
+    }
+    node.appendChild(select);
+
+}
+buildList(beer, data);
+
+
+
+function hasAttr() {
+    var attr = document.getElementsByTagName('input');
+    for (var i = 0; i < attr.length; i++) {
+        if (attr[i].hasAttribute('required') && !attr[i].value) {
+            attr[i].style.borderColor = "yellow";
+        }
+
+    }
+
+}
+
+hasAttr();
