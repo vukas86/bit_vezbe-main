00bfb15317f6bda0a641fb6dd0e7f1ccd8fbd28a
“BIT <vukasbit@gmail.com>
2021-05-31 17:52:58 +0200

    events practice

---
+++ events/main.js
@@ -0,0 +1,61 @@
+/* /* function changeBG() {
+    var a = document.querySelector("body");
+    a.classList.toggle('gray');
+}
+
+function removeButton() {
+
+    var b = document.querySelector(".but");
+    b.removeAttribute("onclick")
+
+
+}
+
+
+
+changeBg();
+
+removeButton(); */
+
+/* function writeMess() {
+    var a = document.createElement("p");
+    var b = document.createTextNode("")
+    b.appendChild(a);
+    document.getElementById("area").appendChild(a);
+}
+
+writeMess() */
+
+/* var button = document.querySelector("button");
+var division = document.querySelector("div");
+
+var input = document.querySelector("input");
+
+button.onclick = function () {
+    var textInput = input.value;
+    var b = document.createElement("p");
+    b.textContent = textInput;
+    division.appendChild(b);
+    input.value = "";
+
+} */
+
+/* var player = document.querySelector("img");*/
+function playerClick() {
+    document.querySelector('body').addEventListener('click', divMove);
+}
+function divMove(e) {
+    var div = document.getElementById('image');
+    div.style.position = 'absolute';
+    div.style.top = e.clientY + "px";
+    div.style.left = e.clientX + "px";
+
+}
+
+playerClick();
+
+
+
+
+
+
--- weeklyProject/footballTeam/js/main.js
+++ weeklyProject/footballTeam/js/main.js
@@ -1,96 +1,111 @@
-var sartid = {players: [
-    {
-     img:"../assets/images/players/player1.png",
-     name:"S. Gentiletti",
-     age:30,
-     position:"GK",
-     number:1
-    },
-    {img:"../assets/images/players/player2.png",
-     name:"A. Mowatt",
-     age:26,
-     position:"CB",
-     number:2
-    },
-    {img:"../assets/images/players/player3.png",
-    name:"R. Corlu",
-    age:27,
-    position:"CB",
-    number:3
-    },
-    {img:"../assets/images/players/player4.png",
-    name:"S. Farelli",
-    age:19,
-    position:"LB",
-    number:16
-    },
-    {img:"../assets/images/players/player5.png",
-    name:"S. Iacoponi",
-    age:22,
-    position:"RB",
-    number:4
-    },
-    {img:"../assets/images/players/player6.png",
-    name:"B. Maubleu",
-    age:31,
-    position:"DM",
-    number:5
-    },
-    {img:"../assets/images/players/player7.png",
-    name:"F. Mrzljak",
-    age:28,
-    position:"CM",
-    number:6
-    },
-    {img:"../assets/images/players/player8.png",
-    name:"G. Hubert",
-    age:27,
-    position:"RM",
-    number:7
-    },
-    {img:"../assets/images/players/player9.png",
-    name:"M. Amini",
-    age:28,
-    position:"LM",
-    number:8
-    },
-    {img:"../assets/images/players/player10.png",
-    name:"O. Gnjatic",
-    age:30,
-    position:"AM",
-    number:9
-    },
-    {img:"../assets/images/players/player11.png",
-    name:"A. Corryn",
-    age:22,
-    position:"ST",
-    number:10
-    },
-    {img:"../assets/images/players/player12.png",
-    name:"J. Sancho",
-    age:33,
-    position:"GK",
-    number:12
-    },
-    {img:"../assets/images/players/player13.png",
-    name:"J. Obi",
-    age:30,
-    position:"CB",
-    number:13
-    },
-    {img:"../assets/images/players/player14.png",
-    name:"D. Frieseri",
-    age:24,
-    position:"CM",
-    number:14
-    },
-    {img:"../assets/images/players/player16.png",
-    name:"M. Brink",
-    age:23,
-    position:"ST",
-    number:15
-    }
-]
+var sartid = {
+    players: [
+        {
+            img: "../assets/images/players/player1.png",
+            name: "S. Gentiletti",
+            age: 30,
+            position: "GK",
+            number: 1
+        },
+        {
+            img: "../assets/images/players/player2.png",
+            name: "A. Mowatt",
+            age: 26,
+            position: "CB",
+            number: 2
+        },
+        {
+            img: "../assets/images/players/player3.png",
+            name: "R. Corlu",
+            age: 27,
+            position: "CB",
+            number: 3
+        },
+        {
+            img: "../assets/images/players/player4.png",
+            name: "S. Farelli",
+            age: 19,
+            position: "LB",
+            number: 16
+        },
+        {
+            img: "../assets/images/players/player5.png",
+            name: "S. Iacoponi",
+            age: 22,
+            position: "RB",
+            number: 4
+        },
+        {
+            img: "../assets/images/players/player6.png",
+            name: "B. Maubleu",
+            age: 31,
+            position: "DM",
+            number: 5
+        },
+        {
+            img: "../assets/images/players/player7.png",
+            name: "F. Mrzljak",
+            age: 28,
+            position: "CM",
+            number: 6
+        },
+        {
+            img: "../assets/images/players/player8.png",
+            name: "G. Hubert",
+            age: 27,
+            position: "RM",
+            number: 7
+        },
+        {
+            img: "../assets/images/players/player9.png",
+            name: "M. Amini",
+            age: 28,
+            position: "LM",
+            number: 8
+        },
+        {
+            img: "../assets/images/players/player10.png",
+            name: "O. Gnjatic",
+            age: 30,
+            position: "AM",
+            number: 9
+        },
+        {
+            img: "../assets/images/players/player11.png",
+            name: "A. Corryn",
+            age: 22,
+            position: "ST",
+            number: 10
+        },
+        {
+            img: "../assets/images/players/player12.png",
+            name: "J. Sancho",
+            age: 33,
+            position: "GK",
+            number: 12
+        },
+        {
+            img: "../assets/images/players/player13.png",
+            name: "J. Obi",
+            age: 30,
+            position: "CB",
+            number: 13
+        },
+        {
+            img: "../assets/images/players/player14.png",
+            name: "D. Frieseri",
+            age: 24,
+            position: "CM",
+            number: 14
+        },
+        {
+            img: "../assets/images/players/player16.png",
+            name: "M. Brink",
+            age: 23,
+            position: "ST",
+            number: 15
+        }
+    ]
 };
 
 
@@ -115,11 +130,11 @@ document.getElementById("team").appendChild(subs);
 var firstTeam = 11;
 var substitutions = 4;
 
-function addPlayers (){
-    for (var i = 0; i<firstTeam + substitutions; i++){
-        if(i<firstTeam) {
-           parentDiv= players;
-        }else {
+function addPlayers() {
+    for (var i = 0; i < firstTeam + substitutions; i++) {
+        if (i < firstTeam) {
+            parentDiv = players;
+        } else {
             parentDiv = subs;
         }
 
@@ -152,11 +167,30 @@ function addPlayers (){
 
 
 
-function getRandNum(arr)  {
-    return Math.floor(Math.random()*arr.length);
+function getRandNum(arr) {
+    return Math.floor(Math.random() * arr.length);
 }
 
-function playerSubstitution () {
+function zamena() {
+    var firstSquad = document.querySelectorAll("#players .player");
+    var firstTeamNumber = getRandNum(firstSquad);
+    var sub1 = firstSquad[firstTeamNumber];
+
+    var reserveSquad = document.querySelectorAll("#subs .player");
+    var reserveSquadNumber = getRandNum(reserveSquad);
+    var sub2 = reserveSquad[reserveSquadNumber];
+
+    players.appendChild(sub1);
+    subs.appendChild(sub2);
+
+    firstSquadPlayer.after(reserveSquadPlayer);
+
+}
+/* function getRandNum(arr) {
+    return Math.floor(Math.random() * arr.length);
+}
+
+function playerSubstitution() {
     var firstSquad = document.querySelectorAll("#players .player");
     var reserveSquad = document.querySelectorAll("#subs .player");
 
@@ -174,9 +208,11 @@ function playerSubstitution () {
     subPrevious ? subPrevious.after(firstSquadPlayer) : subNext.before(firstSquadPlayer);
 }
 
-addPlayers();
-playerSubstitution();
+ */
 
-setInterval(playerSubstitution, 1500);
+addPlayers();
+zamena();
+
+setInterval(zamena, 1000);
 
 
